be477e7d3e56ec890b626d41af410f27d3025301
brainstation <clearlythuydoan@gmail.com>
2017-01-31 15:24:44 -0500

    change from const to class

--- large-project/src/containers/Movies/movies.js
+++ large-project/src/containers/Movies/movies.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
 // Q. How to have it display nothing when no search has been performed?
 // a) Some condition linked to a different state change?
 
-const Movies = (props) => (
-    <div>
+export default class Movies extends Component {
+    render() {
+        return (<div>
         <h3>Search Results</h3>
         <ul>
-            {props.movies.length !== 0 ? props.movies.map((movie, index) => 
-                <Link onClick={(eventToIgnore) => props.click(movie)} to="/movie" key={index}><li>{movie.Title}</li></Link>
+            {this.props.movies.length !== 0 ? this.props.movies.map((movie, index) => 
+                <Link onClick={(eventToIgnore) => this.props.click(movie)} to="/movie" key={index}><li>{movie.Title}</li></Link>
             ) : <li>Results have yet been found but we're optimistic for next time.</li>}
         </ul>
     </div>)
+    }
+}
 
-export default Movies;
+// const Movies = (props) => (
+//     <div>
+//         <h3>Search Results</h3>
+//         <ul>
+//             {props.movies.length !== 0 ? props.movies.map((movie, index) => 
+//                 <Link onClick={(eventToIgnore) => props.click(movie)} to="/movie" key={index}><li>{movie.Title}</li></Link>
+//             ) : <li>Results have yet been found but we're optimistic for next time.</li>}
+//         </ul>
+//     </div>)
+
+// export default Movies;
 
 
