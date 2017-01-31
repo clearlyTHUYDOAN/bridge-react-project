3f3762a5f5230d2425bf7d677526b4c394dc0f08
brainstation <clearlythuydoan@gmail.com>
2017-01-31 14:59:36 -0500

    update

--- large-project/src/containers/Movies/movies.js
+++ large-project/src/containers/Movies/movies.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router';
-// Ternary is not working properly when I add it. Always tries to run second option?
+// Q. How to have it display nothing when no search has been performed?
 
 const Movies = (props) => (
     <div>
         <h3>Search Results</h3>
         <ul>
             {props.movies.length !== 0 ? props.movies.map((movie, index) => 
-                <Link onClick={(eventToIgnore) => props.click(movie)} to="/movie"  key={index}><li>{movie.Title}</li></Link>
+                <Link onClick={(eventToIgnore) => props.click(movie)} to="/movie" key={index}><li>{movie.Title}</li></Link>
             ) : <li>Results have yet been found but we're optimistic for next time.</li>}
         </ul>
     </div>)
