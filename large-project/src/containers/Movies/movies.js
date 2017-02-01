a37b1ec4a3d3909a52931dcc94c27cf75eca65ac
clearlyTHUYDOAN <clearlythuydoan@gmail.com>
2017-01-31 22:36:40 -0500

    complete

--- large-project/src/containers/Movies/movies.js
+++ large-project/src/containers/Movies/movies.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 // Q. How to have it display nothing when no search has been performed?
-// a) Some condition linked to a different state change?
+// a) Some condition linked to a different state change or a combination of state changes?
 
 export default class Movies extends Component {
     render() {
